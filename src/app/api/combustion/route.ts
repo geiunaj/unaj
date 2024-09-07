88b836a92a5811690483cea3b9f8a45e113c1d83
Cristina Brunella Céspedes Malca <ccespedesma@unprg.edu.pe>
2024-09-07 13:26:46 -0500

    prueba

--- src/app/api/combustion/route.ts
+++ src/app/api/combustion/route.ts
@@ -7,7 +7,6 @@ import {getAnioId} from "@/lib/utils";
 
 // INDEX
 export async function GET(req: NextRequest): Promise<NextResponse> {
-    console.log("DATABASE_URL:", process.env.DATABASE_URL);
     try {
         const {searchParams} = new URL(req.url);
         const tipo = searchParams.get("tipo") ?? undefined;
