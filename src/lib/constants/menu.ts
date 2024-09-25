d60cf8185734d6d0f0903dcfc36dd800ec7c531b
Cristina Brunella Céspedes Malca <91429548+CrisCespedes21@users.noreply.github.com>
2024-09-25 14:52:52 -0500

    Update menu.ts

--- src/lib/constants/menu.ts
+++ src/lib/constants/menu.ts
@@ -1,163 +1,192 @@
 import {
-    Home,
-    Truck,
-    Flame,
-    Droplet,
-    Zap,
-    FileText,
-    Bean,
-    Car,
-    Scroll,
-    Sprout,
-    Settings,
-    MapPinned,
-    Droplets,
-    CarTaxiFront,
-    UserRound,
-    Gauge,
-    FlaskConical,
-    PlugZap,
-    Milk,
+  Home,
+  Truck,
+  Flame,
+  Droplet,
+  Zap,
+  FileText,
+  Bean,
+  Car,
+  Scroll,
+  Sprout,
+  Settings,
+  MapPinned,
+  Droplets,
+  CarTaxiFront,
+  UserRound,
+  Gauge,
+  FlaskConical,
+  PlugZap,
+  Milk,
+  StretchHorizontal,
 } from "lucide-react";
 
 export interface MenuItem {
-    title: string;
-    icon: string;
-    href: string;
-    items?: MenuItem[];
+  title: string;
+  icon: string;
+  href: string;
+  items?: MenuItem[];
 }
 
 export const iconComponents: Record<string, any> = {
-    Home,
-    Flame,
-    Truck,
-    Droplet,
-    Zap,
-    FileText,
-    Bean,
-    Car,
-    Scroll,
-    Sprout,
-    Settings,
-    MapPinned,
-    Droplets,
-    CarTaxiFront,
-    UserRound,
-    Gauge,
-    FlaskConical,
-    PlugZap,
-    Milk,
+  Home,
+  Flame,
+  Truck,
+  Droplet,
+  Zap,
+  FileText,
+  Bean,
+  Car,
+  Scroll,
+  Sprout,
+  Settings,
+  MapPinned,
+  Droplets,
+  CarTaxiFront,
+  UserRound,
+  Gauge,
+  FlaskConical,
+  PlugZap,
+  Milk,
+  StretchHorizontal,
 };
 
 export const menu: MenuItem[] = [
-    {
-        title: "Inicio",
-        icon: "Home",
-        href: "/home",
-    },
-    {
-        title: "Combustión Estacionaria",
-        icon: "Flame",
-        href: "/combustion-estacionaria",
-    },
-    {
+  {
+    title: "Inicio",
+    icon: "Home",
+    href: "/home",
+  },
+  {
+    title: "Categoria 1",
+    icon: "StretchHorizontal",
+    href: "/",
+    items: [
+      {
         title: "Combustión Móvil",
         icon: "Car",
         href: "/combustion-movil",
-    },
-    {
+      },
+      {
+        title: "Combustión Estacionaria",
+        icon: "Flame",
+        href: "/combustion-estacionaria",
+      },
+      {
         title: "Fertilizante",
         icon: "Bean",
         href: "/fertilizante",
-    },
-    {
+      },
+    ],
+  },
+  {
+    title: "Categoria 2",
+    icon: "StretchHorizontal",
+    href: "/",
+    items: [
+      {
         title: "Consumo de Electricidad",
         icon: "Zap",
         href: "/electricidad",
-    },
-    {
-        title: "Consumo de Agua",
-        icon: "Droplets",
-        href: "/consumoAgua",
-    },
-    {
+      },
+    ],
+  },
+  {
+    title: "Categoria 3",
+    icon: "StretchHorizontal",
+    href: "/",
+    items: [
+      {
+        title: "Taxis",
+        icon: "CarTaxiFront",
+        href: "/taxi",
+      },
+    ],
+  },
+  {
+    title: "Categoria 4",
+    icon: "StretchHorizontal",
+    href: "/",
+    items: [
+      {
         title: "Consumo de Papel",
         icon: "FileText",
         href: "/papel",
-    },
-    {
+      },
+      {
+        title: "Consumo de Agua",
+        icon: "Droplets",
+        href: "/consumoAgua",
+      },
+      {
         title: "Activos Fijos",
         icon: "FileText",
         href: "/activos",
-    },
-    {
+      },
+      {
         title: "Consumibles Generales",
         icon: "Milk",
         href: "/consumible",
-    },
-    {
-        title: "Taxis Contratados",
-        icon: "CarTaxiFront",
-        href: "/taxi",
-    },
-    {
-        title: "Factores",
-        icon: "FlaskConical",
-        href: "/",
-        items: [
-            {
-                title: "Factores de Combustible",
-                icon: "Flame",
-                href: "/tipo-combustible-factor",
-            },
-            {
-                title: "Factor de Fertilizante",
-                icon: "Sprout",
-                href: "/fertilizante-factor",
-            },
-            {
-                title: "Factor de Emision SEIN",
-                icon: "PlugZap",
-                href: "/factor-emision-SEIN",
-            }
-        ],
-    },
-    {
-        title: "Configuraciones",
-        icon: "Settings",
-        href: "/settings",
-        items: [
-            {
-                title: "Tipo de Papel",
-                icon: "Scroll",
-                href: "/tipoPapel",
-            },
-            {
-                title: "Tipos de Combustible",
-                icon: "Flame",
-                href: "/tipo-combustible",
-            },
-            {
-                title: "Tipos de Fertilizantes",
-                icon: "Sprout",
-                href: "/tipo-fertilizante",
-            },
-            {
-                title: "Area",
-                icon: "MapPinned",
-                href: "/area",
-            },
-            {
-                title: "GPW",
-                icon: "Gauge",
-                href: "/gpw",
-            },
-            {
-                title: "Usuarios",
-                icon: "UserRound",
-                href: "/user",
-            },
-        ]
-    },
-
+      },
+    ],
+  },
+  {
+    title: "Factores",
+    icon: "FlaskConical",
+    href: "/",
+    items: [
+      {
+        title: "Factores de Combustible",
+        icon: "Flame",
+        href: "/tipo-combustible-factor",
+      },
+      {
+        title: "Factor de Fertilizante",
+        icon: "Sprout",
+        href: "/fertilizante-factor",
+      },
+      {
+        title: "Factor de Emision SEIN",
+        icon: "PlugZap",
+        href: "/factor-emision-SEIN",
+      },
+    ],
+  },
+  {
+    title: "Configuraciones",
+    icon: "Settings",
+    href: "/settings",
+    items: [
+      {
+        title: "Tipo de Papel",
+        icon: "Scroll",
+        href: "/tipoPapel",
+      },
+      {
+        title: "Tipos de Combustible",
+        icon: "Flame",
+        href: "/tipo-combustible",
+      },
+      {
+        title: "Tipos de Fertilizantes",
+        icon: "Sprout",
+        href: "/tipo-fertilizante",
+      },
+      {
+        title: "Area",
+        icon: "MapPinned",
+        href: "/area",
+      },
+      {
+        title: "GPW",
+        icon: "Gauge",
+        href: "/gpw",
+      },
+      {
+        title: "Usuarios",
+        icon: "UserRound",
+        href: "/user",
+      },
+    ],
+  },
 ];
