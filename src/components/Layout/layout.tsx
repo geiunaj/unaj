530c188ab3279ac720d3906fd9f8d988bdd77895
david3010M <hvaldiviezos@unprg.edu.pe>
2024-11-26 22:43:40 -0500

    Update layout.tsx

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -29,7 +29,7 @@ const LayoutWrapper = ({children}: { children: React.ReactNode }) => {
         disableTransitionOnChange
     >
         {
-            true ? (
+            session ? (
                 <div className="sm:grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[260px_1fr]">
                     <div className="hidden border-r bg-muted/40 md:block">
                         <Sidebar/>
