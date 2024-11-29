cd52410d2078f09c2d35270b9c4cea978cca35b5
david3010M <hvaldiviezos@unprg.edu.pe>
2024-11-29 00:18:31 -0500

    Update area.interface.ts

--- src/components/area/services/area.interface.ts
+++ src/components/area/services/area.interface.ts
@@ -1,18 +1,19 @@
 export interface Area {
-    id: number;
-    nombre: string;
+  id: number;
+  nombre: string;
+  sede: string;
 }
 
 export interface AreaRequest {
-    nombre: string;
-    sedeId: number;
+  nombre: string;
+  sedeId: number;
 }
 
 export interface CreateAreaProps {
-    onClose: () => void;
+  onClose: () => void;
 }
 
 export interface UpdateAreaProps {
-    id: number;
-    onClose: () => void;
+  id: number;
+  onClose: () => void;
 }
